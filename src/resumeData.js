475eb215414f54da0bccbc777bf818776bfd872b
Shiwei Yu <shiwei1991@gmail.com>
2021-04-19 19:32:24 +0200

    first commit on portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+  imagebaseurl: 'https://numaga94.github.io/portfolio/',
+  name: 'Yu',
+  role: 'I am an entrepreneur and a full stack web developer based in Paris.',
+  linkedinId: 'Your LinkedIn Id',
+  skypeid: 'shiwei.yu1',
+  socialLinks: [
+    {
+      name: 'github',
+      url: 'https://github.com/numaga94',
+      className: 'fa fa-github',
+    },
+    // {
+    //   name: 'skype',
+    //   url: 'https://www.linkedin.com/in/rahul-bhatia-67ba08121/',
+    //   className: 'fa fa-skype',
+    // },
+  ],
+  aboutme:
+    'I am a veteran of Légion étrangère, served as a légionnaire in 11e brigade parachutiste for 5 years. Founded NUMAGA in 2020, a startup company specialized in web development. Welcome contacting me for any web projects that you are about to start. All your demands of the project will be accomplished with honor and fidelity',
+  address: 'India',
+  website: 'https://rbhatia46.github.io',
+  education: [
+    {
+      UniversityName: 'The LNM Insitute of Information Technology',
+      specialization: 'Some specialization',
+      MonthOfPassing: 'Aug',
+      YearOfPassing: '2020',
+      Achievements: 'Some Achievements',
+    },
+    {
+      UniversityName: 'Some University',
+      specialization: 'Some specialization',
+      MonthOfPassing: 'Jan',
+      YearOfPassing: '2018',
+      Achievements: 'Some Achievements',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'Some Company',
+      specialization: 'Some specialization',
+      MonthOfLeaving: 'Jan',
+      YearOfLeaving: '2018',
+      Achievements: 'Some Achievements',
+    },
+    {
+      CompanyName: 'Some Company',
+      specialization: 'Some specialization',
+      MonthOfLeaving: 'Jan',
+      YearOfLeaving: '2018',
+      Achievements: 'Some Achievements',
+    },
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5',
+    },
+    {
+      skillname: 'CSS',
+    },
+    {
+      skillname: 'Reactjs',
+    },
+  ],
+  portfolio: [
+    {
+      name: 'project1',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/coffee.jpg',
+    },
+    {
+      name: 'project2',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/console.jpg',
+    },
+    {
+      name: 'project3',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/retrocam.jpg',
+    },
+    {
+      name: 'project4',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/farmerboy.jpg',
+    },
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy',
+    },
+  ],
+};
+
+export default resumeData;
